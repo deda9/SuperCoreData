6a6f7f294eaac65fd69dd0b059b13d25689f4dc7
deda9 <Bassem.Qoulta@gmail.com>
2018-11-13 17:05:56 +0100

    Create NSManagedObject+Super

---
+++ Source/NSManagedObject+Super.swift
@@ -0,0 +1,145 @@
+import CoreData
+
+// MARK: - Entity Name
+extension NSManagedObject {
+    class var entityName: String {
+        return String(describing: self).components(separatedBy: ".").last!
+    }
+}
+
+// MARK: - Create
+extension NSManagedObject {
+    class func createEntity(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) -> NSManagedObject{
+        let entity = NSEntityDescription.insertNewObject(forEntityName: NSManagedObject.entityName, into: context)
+        return entity
+    }
+}
+
+// MARK: - Delete
+extension NSManagedObject {
+    func deleteEntity(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) {
+        context.delete(self)
+    }
+    
+    class func deleteAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) {
+        context.fetchAll(self).forEach{ $0.deleteEntity(in: context) }
+    }
+    
+    class func deleteAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext, predicate: NSPredicate) {
+        context.fetchAll(self, predicate: predicate).forEach{ $0.deleteEntity(in: context) }
+    }
+}
+
+// MARK: - Fetch
+extension NSManagedObject {
+    class func fetchAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) -> [NSManagedObject] {
+        return context.fetchAll(self)
+    }
+    
+    class func fetchAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                        predicate: NSPredicate) -> [NSManagedObject] {
+        return context.fetchAll(self, predicate: predicate)
+    }
+    
+    class func fetchAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                        predicate: NSPredicate,
+                        sortDescriptors: [NSSortDescriptor]) -> [NSManagedObject] {
+        return context.fetchAll(self, predicate: predicate, sortDescriptors: sortDescriptors)
+    }
+    
+    class func fetchAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                        predicate: NSPredicate?,
+                        sortDescriptors: [NSSortDescriptor],
+                        limit: Int) -> [NSManagedObject] {
+        return context.fetchAll(self, predicate: predicate, sortDescriptors: sortDescriptors, limit: limit)
+    }
+    
+    class func fetchAll(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                        predicate: NSPredicate,
+                        sortDescriptors: [NSSortDescriptor],
+                        limit: Int,
+                        batchSize: Int) -> [NSManagedObject] {
+        return context.fetchAll(self, predicate: predicate, sortDescriptors: sortDescriptors,
+                                limit: limit, batchSize: batchSize)
+    }
+}
+
+// MARK: - Fetch First
+extension NSManagedObject {
+    class func fetchFirst(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) -> NSManagedObject? {
+        return context.fetchFirst(self)
+    }
+    
+    class func fetchFirst(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                          predicate: NSPredicate) -> NSManagedObject? {
+        return context.fetchFirst(self, predicate: predicate)
+    }
+    
+    class func fetchFirst(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                          predicate: NSPredicate,
+                          sortDescriptors: [NSSortDescriptor]) -> NSManagedObject? {
+        return context.fetchFirst(self, predicate: predicate, sortDescriptors: sortDescriptors)
+    }
+    
+    class func fetchFirst(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                          predicate: NSPredicate,
+                          sortDescriptors: [NSSortDescriptor],
+                          limit: Int) -> NSManagedObject? {
+        return context.fetchFirst(self, predicate: predicate, sortDescriptors: sortDescriptors, limit: limit)
+    }
+    
+    class func fetchFirst(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                          predicate: NSPredicate,
+                          sortDescriptors: [NSSortDescriptor],
+                          limit: Int,
+                          batchSize: Int) -> NSManagedObject? {
+        return context.fetchFirst(self, predicate: predicate, sortDescriptors: sortDescriptors,
+                                  limit: limit, batchSize: batchSize)
+    }
+}
+
+// MARK: - Fetch Last
+extension NSManagedObject {
+    class func fetchLast(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) -> NSManagedObject? {
+        return context.fetchLast(self)
+    }
+    
+    class func fetchLast(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                         predicate: NSPredicate) -> NSManagedObject? {
+        return context.fetchLast(self, predicate: predicate)
+    }
+    
+    class func fetchLast(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                         predicate: NSPredicate,
+                         sortDescriptors: [NSSortDescriptor]) -> NSManagedObject? {
+        return context.fetchLast(self, predicate: predicate, sortDescriptors: sortDescriptors)
+    }
+    
+    class func fetchLast(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                         predicate: NSPredicate,
+                         sortDescriptors: [NSSortDescriptor],
+                         limit: Int) -> NSManagedObject? {
+        return context.fetchLast(self, predicate: predicate, sortDescriptors: sortDescriptors, limit: limit)
+    }
+    
+    class func fetchLast(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                         predicate: NSPredicate,
+                         sortDescriptors: [NSSortDescriptor],
+                         limit: Int,
+                         batchSize: Int) -> NSManagedObject? {
+        return context.fetchLast(self, predicate: predicate, sortDescriptors: sortDescriptors,
+                                 limit: limit, batchSize: batchSize)
+    }
+}
+
+// MARK: - Fetch Last
+extension NSManagedObject {
+    class func count(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext) -> Int {
+        return context.fetchAll(self).count
+    }
+    
+    class func count(in context: NSManagedObjectContext = SuperCoreData.default.defaultContext,
+                     predicate: NSPredicate) -> Int {
+        return context.fetchAll(self, predicate:predicate).count
+    }
+}
