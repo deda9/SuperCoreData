0e526a8d19a17ad10039af5dd58255466c19dafe
deda9 <Bassem.Qoulta@gmail.com>
2018-11-13 17:06:48 +0100

    Add Project

---
+++ SuperCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCoreData
+//
+//  Created by Bassem Qoulta on 11/13/18.
+//  Copyright © 2018 Bassem Qoulta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
